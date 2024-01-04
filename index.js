627e1be3f6eb7f0021f7a5392f0963db089f5811
Capti <captianbeau@gmail.com>
2024-01-03 19:13:56 -0700

    base of inquirer

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// if statements to make logos from the results of the questions
+// if statements to make logos from the results of the questions (errors)
+// shapes:circle,square,triangle  colors, text, font
+
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type:'input',
+            message:'Type logo text.',
+            name:'lText',
+        },
+        {
+            type:'',
+            message:'',
+            name:'shapes',
+        }, {
+            type:'',
+            message:'',
+            name:'color',
+        }, {
+            type:'',
+            message:'',
+            name:'font',
+        },
+    ])
+    .then((logoParts) =>
+    console.log(logoParts)
+    );
